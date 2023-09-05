b6fae48f77956bea12cc9f504b3a86e7340b4c47
Rodolfo Santos <rodolfodossantos29@gmail.com>
2023-09-05 16:28:10 -0300

    style: prettier is added to force convetional style

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 140,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  semi: true,
+  embeddedLanguageFormatting: 'auto'
+}
